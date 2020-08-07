58a1d28cfae8e28ef42cea2e7a8e15905db293c1
Javkhaa00 <liplicker0423@gmail.com>
2020-08-07 16:15:10 +0800

    feat: added header section

--- src/containers/header.js
+++ src/containers/header.js
@@ -1,10 +1,26 @@
 import React from 'react';
 
-export const Header = () => {
+const Header = () => {
     return (
-        <>
-            <h1>HELLO WORLD</h1>
-        </>
+        <div className="header">
+            <div className="wrapper">
+                <div className="header--nav">
+                    <div className="header--nav--logo">
+                        <img className="header--nav--logo--img" alt="nest-logo" src="/assets/logo-white.png" />
+                    </div>
+                    <div className="header--nav--buttons">
+                        <ul>
+                            <li><i href="#">НҮҮР</i></li>
+                            <li><i href="#">Бидний тухай</i></li>
+                            <li><i href="#">Хөтөлбөр</i></li>
+                            <li><i href="#">Багш нар</i></li>
+                            <li><i href="#">Мэдээ</i></li>
+                            <li><i href="#">Холбоо барих</i></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 };
 
