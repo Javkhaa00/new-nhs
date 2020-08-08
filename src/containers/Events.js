2a3016dae19aef6a2d3a26f8a3d618eb531d20b7
Javkhaa00 <liplicker0423@gmail.com>
2020-08-08 13:50:36 +0800

    feat: added Events section with scss

---
+++ src/containers/Events.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+export const Events = () => {
+    return (
+        <div className="events">
+            <span className="events--title">Нэстээр сонин сайхан</span>
+            <div className="events--wrapper">
+                <span className="events--wrapper--desc">
+                    Жигүүртэн шувууд лугаа адил эрх чөлөөтэй нисэж, тэнгэрийг эзэгнэх чухам тэр чин хүсэл эрмэлзэл биднийг гэнэхэн мөрөөдөгчдөөс дэлхийг.
+                </span>
+                <div className="events--wrapper--every">
+                    <div className="events--wrapper--every--button">Бүгд</div>
+                </div>
+            </div>
+            <div className="events--all-cards">
+                <div className="card">
+                    <div className="card--img1">
+
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="card--img1">
+
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="card--img1">
+
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="card--img1">
+
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="card--img1">
+
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="card--img1">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Events;
