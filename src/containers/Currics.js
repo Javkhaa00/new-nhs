cad6b9df2e2d8eb7dd47f4265d91beaa2563fb0a
Javkhaa00 <liplicker0423@gmail.com>
2020-08-11 15:21:01 +0800

    feat: added real pics and description

--- src/containers/Currics.js
+++ src/containers/Currics.js
@@ -12,10 +12,72 @@ export const Currics = () => {
                 </p>
             </div>
             <div className="currics--img-container">
-                <div className="currics--img-container--imgs"></div>
-                <div className="currics--img-container--imgs1"></div>
-                <div className="currics--img-container--imgs2"></div>
-                <div className="currics--img-container--imgs3"></div>
+                <div className="currics--row">
+                    <div className="currics--img-container--imgs">
+                        <div className="text-container">
+                            <div className="text-container--bold">
+                                ЕБ баяжуулсан хөтөлбөр
+                            </div>
+                            <div className="text-container--text">
+                                Боловсролын яамнаас гаргасан ЕБС-ийн баяжуулсан хөтөлбөр
+                            </div>
+                            <div className="text-container--text">
+                                Анги дүүргэлт 18 сурагч
+                            </div>
+                            <div className="text-container--text">
+                                Эрүүл амьдралын дадал хэвшүүлэх цогц хөтөлбөр
+                            </div>
+                        </div>
+                    </div>
+                    <div className="currics--img-container--imgs1">
+                        <div className="text-container">
+                            <div className="text-container--bold">IT хөтөлбөр</div>
+                            <div className="text-container--text">
+                                Олон улсын жишигт нийцсэн мэдээллийн технологийн 1200+ цагийн цогц хөтөлбөр
+                            </div>
+                            <div className="text-container--text">
+                                Монголын тэргүүлэх компаниудад дадлага хийх, дэлхийн томоохон компаниудтай танилцах хөтөлбөр
+                            </div>
+                            <div className="text-container--text">
+                                Олон улсын зөвлөх баг
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="currics--row">
+                    <div className="currics--img-container--imgs2">
+                        <div className="text-container">
+                            <div className="text-container--bold">
+                                Фитнес - эрүүл чийрэг бие
+                            </div>
+                            <div className="text-container--text">
+                                Сурагч бүрийн биеийн онцлогт тохирсон чийрэгжүүлэх хөтөлбөр
+                            </div>
+                            <div className="text-container--text">
+                                8 шүршүүр, эмчийн өрөө, хувцас солих өрөө болон кросфит бүрэн тоноглогдсон заал
+                            </div>
+                            <div className="text-container--text">
+                                Эмэгтэй сурагчдад зориулсан иога, гимнастикийн хөтөлбөр
+                            </div>
+                        </div>
+                    </div>
+                    <div className="currics--img-container--imgs3">
+                        <div className="text-container">
+                            <div className="text-container--bold">Нэмэлт хөтөлбөр</div>
+                            <div className="text-container--text">Англи, Хятад хэл</div>
+                            <div className="text-container--text">Сургалтын конслер</div>
+                            <div className="text-container--text">
+                                Фото зургийн дугуйлан
+                            </div>
+                            <div className="text-container--text">
+                                Рубик шооны дугуйлан
+                            </div>
+                            <div className="text-container--text">
+                                Мэдээллийн аюулгүй байдлын дугуйлан
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
