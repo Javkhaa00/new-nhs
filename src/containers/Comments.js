55402b928902bcba483e7130e44735db3f9b6ccf
Javkhaa00 <liplicker0423@gmail.com>
2020-08-11 15:19:26 +0800

    feat: comment section js and scss

---
+++ src/containers/Comments.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Comments = () => {
+    return (
+        <div className="comments">
+            <h1>DWA</h1>
+        </div>
+    );
+};
+
+export default Comments;
