fc84214cdd10d2052ecebecbbec2bf089d859deb
Javkhaa00 <liplicker0423@gmail.com>
2020-08-12 18:21:57 +0800

    feat: fixed comments and added swiper

--- src/containers/Comments.js
+++ src/containers/Comments.js
@@ -1,27 +1,45 @@
 import React from 'react';
-import { CommentsCards } from "../components/CommentsCards";
 import { Swiper, SwiperSlide } from 'swiper/react';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import SwiperCore, { Navigation, Pagination, Controller } from 'swiper';
 import 'swiper/swiper-bundle.css';
+import comments from "../json/comments.json";
 
 SwiperCore.use([Navigation, Pagination, Controller]);
 
 export const Comments = () => {
+
+    const renderCards = (el) => {
+        return (
+            <SwiperSlide>
+                <div className="comments--card">
+                    <div className="comments--row">
+                        <div className="comments--card--img">
+                            <img alt=" " src={el.imgUrl} />
+                        </div>
+                        <div className="comments--card--column">
+                            <div className="comments--card--column--name">{el.name}</div>
+                            <div className="comments--card--column--position">{el.position}</div>
+                        </div>
+                    </div>
+                    <div className="comments--card--text">{el.text}</div>
+                </div>
+            </SwiperSlide>
+        );
+    }
+
     return (
-        <div className="comments">
-            <CommentsCards />
-            {/* <Swiper
-                loop={true}
-                navigation
-                pagination
-                spaceBetween={20}
-                slidesPerView={3}
-            >
-                <SwiperSlide>SDASD</SwiperSlide>
-                <SwiperSlide>DSADSA</SwiperSlide>
-                <SwiperSlide>DSADAS</SwiperSlide>
-            </Swiper> */}
+        <div className="comments-wrapper">
+            <div className="comments">
+                <Swiper
+                    loop={true}
+                    navigation
+                    slidesPerView={3}
+                >
+                    {comments.map(renderCards)};
+            </Swiper>
         </div>
+        </div >
     );
 };
 
