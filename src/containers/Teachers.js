a489de23a70964b2ddd9659a6829542f50bbd58c
Javkhaa00 <liplicker0423@gmail.com>
2020-08-13 10:05:26 +0800

    feat: added teachers section header

--- src/containers/Teachers.js
+++ src/containers/Teachers.js
@@ -3,7 +3,17 @@ import React from "react";
 export const Teachers = () => {
     return (
         <div className="teachers">
-            <h1>HI</h1>
+            <div className="teachers--header">
+                <div className="teachers--header--where">
+                    Багш нар
+                </div>
+                <div className="teachers--header--title">
+                    Бидний бахархал
+                </div>
+                <div className="teachers--header--desc">
+                    Жигүүртэн шувууд лугаа адил эрх чөлөөтэй нисэж, тэнгэрийг эзэгнэх чухам тэр чин хүсэл эрмэлзэл биднийг гэнэхэн мөрөөдөгчдөөс дэлхийг.
+                </div>
+            </div>
         </div>
     )
 }
